4c1dbaef7469a647d65c8db58f107c7d991fc194
Laura Peltonen <laura.peltonen53@gmail.com>
2023-10-21 23:35:39 +0300

    2.4 done

--- osa2/kurssitiedot/src/components/Course.jsx
+++ osa2/kurssitiedot/src/components/Course.jsx
@@ -11,7 +11,7 @@ const Course = ({course}) => {
 const Header = ({course}) => {
     return (
         <>
-            <h1>{course.name}</h1>
+            <h2>{course.name}</h2>
         </>
     )
 }
